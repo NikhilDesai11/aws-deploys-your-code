4f528b6cb008c8fde9b7c22030a73541b3d8373a
EC2 Default User <ec2-user@ip-172-31-47-191.ap-south-1.compute.internal>
2026-05-21 16:16:45 +0000

    Modified CodeDeploy files

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -4,12 +4,11 @@ sudo yum -y install httpd
 sudo cat << EOF > /etc/httpd/conf.d/tomcat_manager.conf
 <VirtualHost *:80>
   ServerAdmin root@localhost
-  ServerName app.icl.com
+  ServerName app.nextwork.com
   DefaultType text/html
   ProxyRequests off
   ProxyPreserveHost On
-  ProxyPass / http://localhost:8080/icl-web-project/
-  ProxyPassReverse / http://localhost:8080/icl-web-project/
+  ProxyPass / http://localhost:8080/nextwork-web-project/
+  ProxyPassReverse / http://localhost:8080/nextwork-web-project/
 </VirtualHost>
 EOF
-
